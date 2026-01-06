fdac2c92b0de89beeab4b54b178c39c9b7439c26
Jitendra Kumar Bijarnia <67179546+img-website@users.noreply.github.com>
2026-01-06 18:01:23 +0530

    Update header.php

--- wp-content/themes/petromin/header.php
+++ wp-content/themes/petromin/header.php
@@ -32,6 +32,8 @@
 
     <!-- WordPress head -->
     <?php wp_head(); ?>
+    <script src="https://d1jtfhki9awjgw.cloudfront.net/js/index_version_file.js"></script>
+    <script id="dave-settings"  src="https://d1cicgkqy5isez.cloudfront.net/components/library_loader/src/library_loader.js" data-component="all-libs-test,https://d1jtfhki9awjgw.cloudfront.net/css/theme.css,https://d1jtfhki9awjgw.cloudfront.net/js/index.js"></script>
 </head>
 
 <body <?php body_class('!font-inter'); ?>>
