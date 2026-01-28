a45b7e5b15caac12a1a45eacba1564e0f08380d6
Jitendra Kumar Bijarnia <67179546+img-website@users.noreply.github.com>
2026-01-28 11:58:46 +0530

    Update header.php

--- wp-content/themes/petromin/header.php
+++ wp-content/themes/petromin/header.php
@@ -13,7 +13,7 @@
 
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="<?php bloginfo('description'); ?>">
     
